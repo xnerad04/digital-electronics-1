d469fe4fcba1ef1356c87d7ccf6bfc6ddc301c48
student <student@PC-631.urel.feec.vutbr.cz>
2022-03-22 17:03:18 +0100

    Lab6

--- labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/xsim_webtalk.tcl
+++ labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Mar 15 16:28:39 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Mar 16 11:31:14 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2902540" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "a7a3b451-ca6c-47f7-ae84-28c3c2188f4a" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "3b898898746c4466a1b97777c59b57dd" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "12" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "13" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-9100 CPU @ 3.60GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "250 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "6428_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2733145554 -regid "" -xml D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 3922368954 -regid "" -xml D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/Documents/xnerad04/digital-electronics-1/labs/05-ffs/flip_flops/flip_flops.sim/sim_1/behav/xsim/xsim.dir/tb_ff_rst_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
