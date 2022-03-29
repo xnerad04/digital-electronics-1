1f5b149d43e4d1d2316ed99ceddaf87be76d208a
student <student@PC-631.urel.feec.vutbr.cz>
2022-03-29 16:54:35 +0200

    done

--- labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/driver_7seg_4digits.vhd
+++ labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/driver_7seg_4digits.vhd
@@ -75,7 +75,7 @@ begin
         generic map(
             -- FOR SIMULATION, CHANGE THIS VALUE TO 4
             -- FOR IMPLEMENTATION, KEEP THIS VALUE TO 400,000
-            g_MAX => 4
+            g_MAX => 400000
         )
         port map(
             clk   => clk,
